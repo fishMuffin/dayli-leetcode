793da45efe97f4646c1868691d8b65554c283ae1
Klein <yinchengwen@magiccreative.tech>
2022-06-26 13:00:30 +0800

    1646. 获取生成数组中的最大值

--- src/edu/klein/leetCode.java
+++ src/edu/klein/leetCode.java
@@ -4122,4 +4122,55 @@ public class leetCode {
         }
         return arr[arr.length - 1] + k;
     }
+
+
+    /**
+     * 给你一个整数 n 。按下述规则生成一个长度为 n + 1 的数组 nums ：
+     * <p>
+     * nums[0] = 0
+     * nums[1] = 1
+     * 当 2 <= 2 * i <= n 时，nums[2 * i] = nums[i]
+     * 当 2 <= 2 * i + 1 <= n 时，nums[2 * i + 1] = nums[i] + nums[i + 1]
+     * 返回生成数组 nums 中的 最大 值。
+     * <p>
+     * 输入：n = 7
+     * 输出：3
+     * 解释：根据规则：
+     * nums[0] = 0
+     * nums[1] = 1
+     * nums[(1 * 2) = 2] = nums[1] = 1
+     * nums[(1 * 2) + 1 = 3] = nums[1] + nums[2] = 1 + 1 = 2
+     * nums[(2 * 2) = 4] = nums[2] = 1
+     * nums[(2 * 2) + 1 = 5] = nums[2] + nums[3] = 1 + 2 = 3
+     * nums[(3 * 2) = 6] = nums[3] = 2
+     * nums[(3 * 2) + 1 = 7] = nums[3] + nums[4] = 2 + 1 = 3
+     * 因此，nums = [0,1,1,2,1,3,2,3]，最大值 3
+     * <p>
+     * 提交次数:1
+     * 解决方式: 自我解决
+     * 未来是否需要更优化的解题方法:否
+     * 未来是否需要复盘:否
+     *
+     * @param n
+     * @return
+     */
+    public int getMaximumGenerated(int n) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+        int[] nums = new int[n+1];
+        nums[0] = 0;
+        nums[1] = 1;
+        for (int i = 2; i < nums.length; i++) {
+            if (i % 2 == 0)
+                nums[i] = nums[i / 2];
+            else
+                nums[i] = nums[i / 2] + nums[i / 2 + 1];
+        }
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+
 }
