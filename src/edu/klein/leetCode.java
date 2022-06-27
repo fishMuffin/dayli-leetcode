1374d754cac0ae996783c103fd9c5fe97172b84b
Klein <yinchengwen@magiccreative.tech>
2022-06-27 09:27:17 +0800

    860. 柠檬水找零

--- src/edu/klein/leetCode.java
+++ src/edu/klein/leetCode.java
@@ -4293,4 +4293,54 @@ public class leetCode {
     }
 
 
+    /**
+     * 在柠檬水摊上，每一杯柠檬水的售价为 5 美元。顾客排队购买你的产品，（按账单 bills 支付的顺序）一次购买一杯。
+     * <p>
+     * 每位顾客只买一杯柠檬水，然后向你付 5 美元、10 美元或 20 美元。你必须给每个顾客正确找零，也就是说净交易是每位顾客向你支付 5 美元。
+     * <p>
+     * 注意，一开始你手头没有任何零钱。
+     * <p>
+     * 给你一个整数数组 bills ，其中 bills[i] 是第 i 位顾客付的账。如果你能给每位顾客正确找零，返回 true ，否则返回 false 。
+     * <p>
+     * 输入：bills = [5,5,5,10,20]
+     * 输出：true
+     * 解释：
+     * 前 3 位顾客那里，我们按顺序收取 3 张 5 美元的钞票。
+     * 第 4 位顾客那里，我们收取一张 10 美元的钞票，并返还 5 美元。
+     * 第 5 位顾客那里，我们找还一张 10 美元的钞票和一张 5 美元的钞票。
+     * 由于所有客户都得到了正确的找零，所以我们输出 true。
+     *
+     * <p>
+     * 提交次数:1
+     * 解决方式: 自我解决
+     * 未来是否需要更优化的解题方法:否
+     * 未来是否需要复盘:否
+     *
+     * @param bills
+     * @return
+     */
+    public boolean lemonadeChange(int[] bills) {
+        int[] count = new int[21];
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5) {
+                count[5]++;
+            } else if (bills[i] == 10) {
+                if (count[5] != 0) {
+                    count[5]--;
+                    count[10]++;
+                } else return false;
+            } else if (bills[i] == 20) {
+                if (count[10] != 0) {
+                    count[10]--;
+                    if (count[5] != 0) count[5]--;
+                    else return false;
+                } else {
+                    if (count[5] >= 3) count[5] -= 3;
+                    else return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
