2fc8bcb8653d02b83c955018b2a71573198f8aca
Klein <yinchengwen@magiccreative.tech>
2022-06-30 21:48:40 +0800

    830. 较大分组的位置

--- src/edu/klein/leetCode.java
+++ src/edu/klein/leetCode.java
@@ -5389,7 +5389,7 @@ public class leetCode {
     /**
      * 给你一个字符串数组 words ，数组中的每个字符串都可以看作是一个单词。请你按 任意 顺序返回 words 中是其他单词的子字符串的所有单词。
      * <p>
-     * 如果你可以删除 words[j] 最左侧和/或最右侧的若干字符得到 word[i] ，那么字符串 words[i] 就是 words[j] 的一个子字符串。
+     * 如果你可以删除 words[j] 最左侧和/或最右侧的若干字符得到 word[i] ，那么字符串 words[i] 就是 words[j] 的一个子字符串。
      * <p>
      * 输入：words = ["mass","as","hero","superhero"]
      * 输出：["as","hero"]
@@ -5418,5 +5418,53 @@ public class leetCode {
             i++;
         }
         return ret;
+    }
+
+
+    /**
+     * 在一个由小写字母构成的字符串 s 中，包含由一些连续的相同字符所构成的分组。
+     * <p>
+     * 例如，在字符串 s = "abbxxxxzyy" 中，就含有 "a", "bb", "xxxx", "z" 和 "yy" 这样的一些分组。
+     * <p>
+     * 分组可以用区间 [start, end] 表示，其中 start 和 end 分别表示该分组的起始和终止位置的下标。上例中的 "xxxx" 分组用区间表示为 [3,6] 。
+     * <p>
+     * 我们称所有包含大于或等于三个连续字符的分组为 较大分组 。
+     * <p>
+     * 找到每一个 较大分组 的区间，按起始位置下标递增顺序排序后，返回结果。
+     * <p>
+     * 输入：s = "abcdddeeeeaabbbcd"
+     * 输出：[[3,5],[6,9],[12,14]]
+     * 解释：较大分组为 "ddd", "eeee" 和 "bbb"
+     * <p>
+     * 提交次数:1
+     * 解决方式: 自我解决
+     * 未来是否需要更优化的解题方法:否
+     * 未来是否需要复盘:否
+     *
+     * @param s
+     * @return
+     */
+    public List<List<Integer>> largeGroupPositions(String s) {
+        List<List<Integer>> ret = new ArrayList<>();
+        int start = 0;
+        int end = 1;
+        while (end < s.length()) {
+            if (s.charAt(end) != s.charAt(start)) {
+                setValue(ret, start, end);
+                start = end;
+            }
+            end++;
+        }
+        setValue(ret, start, end);
+        return ret;
+    }
+
+    private void setValue(List<List<Integer>> ret, int start, int end) {
+        if (end - start >= 3) {
+            List<Integer> temp = new ArrayList<>();
+            temp.add(start);
+            temp.add(end-1);
+            ret.add(temp);
+        }
     }
 }
